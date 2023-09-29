5dd3b9447f11762692cd7f87fa8dc67d47807925
Bruno Cantuaria <bruno@cantuaria.net.br>
2023-09-29 17:26:55 -0300

    Fix

--- bin/logdna.sh
+++ bin/logdna.sh
@@ -1,8 +1,3 @@
-echo "[LOG] Installing LOG DNA"
-echo "deb https://assets.logdna.com stable main" | tee /etc/apt/sources.list.d/logdna.list
-wget -O- https://assets.logdna.com/logdna.gpg | apt-key add -
-apt-get update
-apt-get install logdna-agent < "/dev/null" # this line needed for copy/paste
 echo "[LOG] Preparing LOG DNA File"
 touch /etc/logdna.env
 echo "LOGDNA_INGESTION_KEY=a0a084f9598b790ff4a0e14fe79f4c16" | tee /etc/logdna.env # this is your unique Ingestion Key
